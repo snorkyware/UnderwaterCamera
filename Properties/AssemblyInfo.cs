d718a499a1438ec934489da33989f8897d34795e
Snorkybuns <snorkybuns@snorkyware.com>
2024-05-22 19:51:48 +0100

    Initial commit.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnderwaterCamera")]
+[assembly: AssemblyDescription("Valheim game camera mod plugin")]
+[assembly: AssemblyCompany("Snorkyware")]
+[assembly: AssemblyProduct("UnderwaterCamera")]
+[assembly: AssemblyCopyright("Copyright © Snorkyware 2024")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d5949d3-882f-4557-827f-b915610e7753")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.*")]
+
+[assembly: AssemblyMetadata("RepositoryUrl", "https://github.com/snorkyware/UnderwaterCamera")]
